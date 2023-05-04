4756b16cdfe4c67fb25db93a0a25b872c8a48f3c
Sharif Aly <sharif.aly_@outlook.com>
2023-05-04 22:29:13 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  // NewToDO is input value
+  newToDo: any;
+  // list is empty string where i push the value from newToDO
+  list: string[] = [];
+
+  // Get the toDoList from localstorage
+  toDoList = (this.list = JSON.parse(localStorage.getItem('toDos')!));
+
+  // Remove toDo from array and localstorage
+
+  removeToDo(i: any, j: any) {
+    this.list.splice(i, j);
+    localStorage.setItem('toDos', JSON.stringify(this.list));
+    return;
+  }
+
+  // Save ToDo in LocalStorage
+
+  saveLocal() {
+    this.list.push(this.newToDo.toUpperCase());
+    localStorage.setItem('toDos', JSON.stringify(this.list));
+    this.newToDo = '';
+    return;
+  }
+
+  ngOnInit(): void {
+    /*
+    Set default in localstorage on empty array,
+    because is the localstorage is empty,
+    can't push newToDo to localstorage
+    */
+    if (!this.list) {
+      localStorage.setItem('toDos', JSON.stringify((this.list = [])));
+    }
+  }
+}
